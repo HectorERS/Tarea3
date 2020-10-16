8bca5fbf2d21ec94e45681363d504e3ef99a92c6
HectorERS <72960108+HectorERS@users.noreply.github.com>
2020-10-16 16:13:34 -0300

    Update tareaa.py

--- tareaa.py
+++ tareaa.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template
 #la llave  tiene un tamaño maximo de 16 caracteres
 key = b'1234567890123456'
 iv = Random.new().read(CAST.block_size)
-cipher = CAST.new(key, CAST.MODE_OPENPGP, iv)
-plaintext = b'MENSAJE A DESCIFRAR CON EL ALGORITMO CAST'
+cipher = CAST.new(key, CAST.MODE_CFB, iv)
+plaintext = b'mensaje a descifrar con el algoritmo CAST128'
 msg = cipher.encrypt(plaintext)
 
 
