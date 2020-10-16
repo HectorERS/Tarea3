3b176227a0f4bafa8f442a39d6bd6ac8bbf0c666
HectorERS <72960108+HectorERS@users.noreply.github.com>
2020-10-16 17:30:47 -0300

    Update tareaa.py

--- tareaa.py
+++ tareaa.py
@@ -1,16 +1,26 @@
 from Crypto.Cipher import CAST
 from Crypto import Random
 from flask import Flask, render_template
+from base64 import b64encode
 
-
-#la llave  tiene un tamaño maximo de 16 caracteres
-key = b'1234567890123456'
+#i = rondas realizadas, para i<10, 12 rondas, i>10 son 16 rondas
+i=0
+#la llave  tiene un tamaño maximo de 16 caracteres y minimo de 5 caracteres
+key = b'12345'
 iv = Random.new().read(CAST.block_size)
-cipher = CAST.new(key, CAST.MODE_CFB, iv)
-plaintext = b'mensaje a descifrar con el algoritmo CAST128'
-msg = cipher.encrypt(plaintext)
+cipher = CAST.new(key, CAST.MODE_CFB)
+msg= b'mensaje a descifrar con el algoritmo CAST128'
+if len(key)<10:
+    while i != 12:
+        msg = cipher.encrypt(msg)
+        i=i+1
+if len(key)>10:
+    while i != 16:
+        msg = cipher.encrypt(msg)
+        i=i+1
 msg= b64encode(msg).decode('utf-8')
-
+print(len(key))
+print(i)
 app= Flask(__name__)
 
 
@@ -19,7 +29,6 @@ app= Flask(__name__)
 def index():
     id = msg
     return render_template("home.html", id=msg)
-
 
 
 if __name__ == "__main__":
